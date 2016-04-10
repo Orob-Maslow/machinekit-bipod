96fa9c361fde51c1a95874dfd0190fd592803a1e
mattvenn <matt@mattvenn.net>
2016-04-10 20:28:28 +0200

    cd to home

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 export PATH=$PATH:/home/machinekit/bipod/xbee-hal-py/
 export HOME=/home/machinekit/
 export USER=machinekit
-/usr/bin/linuxcnc /home/machinekit/bipod/bipod.ini 
+cd $HOME
+date >> /tmp/start.log 2>&1
+exec /usr/bin/linuxcnc /home/machinekit/bipod/bipod.ini >> /tmp/start.log 2>&1
