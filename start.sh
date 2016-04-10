573b34c8fbba5c7728f159b7347962d6e161b56f
mattvenn <matt@mattvenn.net>
2016-04-10 22:44:49 +0200

    start button at the same time

--- start.sh
+++ start.sh
@@ -4,4 +4,7 @@ export HOME=/home/machinekit/
 export USER=machinekit
 cd $HOME
 date >> /tmp/start.log 2>&1
+# start controller button
+./bipod/button.py &
+# replace process with bipod process
 exec /usr/bin/linuxcnc /home/machinekit/bipod/bipod.ini >> /tmp/start.log 2>&1
