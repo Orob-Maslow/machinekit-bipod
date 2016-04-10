6a87b59f20b246b94e205d94725612c81af60d6a
mattvenn <matt@mattvenn.net>
2016-04-10 20:41:13 +0200

    override with file

--- interrupt.py
+++ interrupt.py
@@ -2,11 +2,20 @@
 couldn't get Adafruit library working reliably
 """
 import select
+import time
 import threading
+import logging
+import os
+
+log = logging.getLogger(__name__)
+
+# file to override button press
+override = "/tmp/button"
 
 class Interrupt(threading.Thread):
 
     def run(self):
+        log.debug("button thread started")
         # hard coded p9.12
         f = '/sys/class/gpio/gpio60/value'
         # open & read the file
@@ -14,9 +23,26 @@ class Interrupt(threading.Thread):
         fh.read()
         poller = select.poll()
         poller.register(fh, select.POLLPRI | select.POLLERR)
-        events = poller.poll()
+
         # wait until something changes and quit
+        while True:
+            events = poller.poll(1)
+            if len(events):
+                log.debug("button event")
+                break
+
+            # check override
+            try:
+                open(override)
+                log.debug("button override")
+                os.unlink(override)
+                break
+            except IOError:
+                pass
+
+        # finish
         fh.close()
+        log.debug("button thread finished")
 
 if __name__ == '__main__':
     thread = Interrupt()
