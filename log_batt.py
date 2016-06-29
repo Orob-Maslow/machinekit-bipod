507d9e4aa5269c9658216a7202f60830c17a53f6
mattvenn <matt@mattvenn.net>
2016-06-29 13:04:18 +0000

    pin names for xbee

--- log_batt.py
+++ log_batt.py
@@ -4,8 +4,12 @@ from subprocess import Popen, PIPE
 import requests
 
 # fetch it
-batt = Popen('halcmd getp xbee.batt', shell=True, stdout=PIPE).stdout.read()
+batt = Popen('halcmd getp xbee.0.batt', shell=True, stdout=PIPE).stdout.read()
 
-# log it
-url = 'http://phant.cursivedata.co.uk/input/L4y9DEbMzEUOdzPYlQ9NSJAXom4?private_key=87y0npg4jpt3xDgLz9YqT5xrPbq&batt=%s' % batt
-r = requests.get(url)
+if batt is None:
+    #error
+    pass
+else:
+    # log it
+    url = 'http://phant.cursivedata.co.uk/input/L4y9DEbMzEUOdzPYlQ9NSJAXom4?private_key=87y0npg4jpt3xDgLz9YqT5xrPbq&batt=%s' % batt
+    r = requests.get(url)
