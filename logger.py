bcf2d59ea4979b25eb3fdded71144222284ab102
mattvenn <matt@mattvenn.net>
2016-07-01 15:35:25 +0200

    log lots more things

--- logger.py
+++ logger.py
@@ -18,22 +18,29 @@ fh.setFormatter(log_format)
 log.addHandler(fh)
 
 log.info("started")
+pins = [
+    { 'pin' : 'axis.0.motor-pos-cmd', 'tag': 'xpos' },
+    { 'pin' : 'axis.1.motor-pos-cmd', 'tag': 'ypos' },
+    { 'pin' : 'axis.2.motor-pos-cmd', 'tag': 'zpos' },
+    { 'pin' : 'xbee.batt', 'tag': 'xbeebatt' },
+    { 'pin' : 'xbee.cksum-err', 'tag': 'xbeecksumerr' },
+    { 'pin' : 'xbee.no-rx-err', 'tag': 'xbeerxerr' },
+    ]
 
+payload = { 'private_key': '87y0npg4jpt3xDgLz9YqT5xrPbq' }
+for pin in pins:
 # fetch it
-p = Popen('halcmd getp xbee.0.batt', shell=True, stdout=PIPE, stderr=PIPE)
-out, err = p.communicate()
-log.debug("error code = %d" % p.returncode)
-log.debug("stderr = %s" % err)
-log.debug("stdout = %s" % out)
+    p = Popen('halcmd getp %s' % pin['pin'], shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, err = p.communicate()
+    log.debug("error code = %d" % p.returncode)
+    log.debug("stderr = %s" % err)
+    log.debug("stdout = %s" % stdout)
 
-if out is None:
-    #error
-    pass
-else:
-    # log it
-    log.info("logging to phant")
-    url = 'http://phant.cursivedata.co.uk/input/L4y9DEbMzEUOdzPYlQ9NSJAXom4?private_key=87y0npg4jpt3xDgLz9YqT5xrPbq&batt=%s' % out
-    r = requests.get(url)
-    log.debug("get status = %d" % r.status_code)
+    payload[pin['tag']] = stdout
 
+# log it
+log.info("logging to phant")
+url = 'http://phant.cursivedata.co.uk/input/L4y9DEbMzEUOdzPYlQ9NSJAXom4'
+r = requests.get(url, params=payload)
+log.debug("get status = %d" % r.status_code)
 log.info("finished")
