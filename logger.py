388a46d38eaa9be700268682b70e1936319707ba
mattvenn <matt@mattvenn.net>
2016-07-12 10:23:56 +0200

    added separate linux feed and started logging autossh pids

--- logger.py
+++ logger.py
@@ -20,6 +20,7 @@ fh = logging.FileHandler('logger.log')
 fh.setFormatter(log_format)
 log.addHandler(fh)
 
+# start with linuxcnc stuff
 payload = { 'private_key': '87y0npg4jpt3xDgLz9YqT5xrPbq' }
 
 log.info("started")
@@ -31,6 +32,8 @@ pins = [
     { 'pin' : 'xbee.gond_err_count', 'tag': 'pen_errcount' },
     { 'pin' : 'xbee.cksum-err', 'tag': 'xbee_cksumerr' },
     { 'pin' : 'xbee.rx-err', 'tag': 'xbee_rxerr' },
+    { 'pin' : 'xbee.gond_touch', 'tag': 'pen_touch' },
+
     ]
 
 # fetch robot data from the HAL
@@ -43,25 +46,62 @@ for pin in pins:
 
     payload[pin['tag']] = stdout
 
-# get uptime
-with open('/proc/uptime', 'r') as f:
-    uptime_seconds = float(f.readline().split()[0])
-    payload["uptime"] = uptime_seconds
-
 # get linuxcnc stats
 import linuxcnc
-sta = linuxcnc.stat()
-sta.poll()
-payload['cnc_execstate'] = sta.exec_state
-payload['cnc_interpstate'] = sta.interp_state
-payload['cnc_state'] = sta.state
-payload['xpos'] = sta.position[0]
-payload['ypos'] = sta.position[1]
-payload['zpos'] = sta.position[2]
+try:
+    sta = linuxcnc.stat()
+    sta.poll()
+    payload['cnc_execstate'] = sta.exec_state
+    payload['cnc_interpstate'] = sta.interp_state
+    payload['cnc_state'] = sta.state
+    payload['xpos'] = sta.position[0]
+    payload['ypos'] = sta.position[1]
+    payload['zpos'] = sta.position[2]
+except Exception as e:
+    log.warning(e)
+    payload['xpos'] = None
+    payload['ypos'] = None
+    payload['zpos'] = None
 
 # log it
 log.info("logging to phant")
 url = 'http://phant.cursivedata.co.uk/input/L4y9DEbMzEUOdzPYlQ9NSJAXom4'
+r = requests.get(url, params=payload)
+log.debug("get status = %d" % r.status_code)
+
+# do the linux stuff
+payload = { "private_key": "45pby8Ab0RcjKmO9qAjYCwmnM8a" }
+
+# get uptime
+with open('/proc/uptime', 'r') as f:
+    uptime_seconds = float(f.readline().split()[0])
+    payload["uptime"] = uptime_seconds
+# load avg
+with open('/proc/loadavg', 'r') as f:
+    load_avg = float(f.readline().split()[0])
+    payload["load_avg"] = load_avg
+# temp
+with open('/sys/class/hwmon/hwmon0/device/temp1_input', 'r') as f:
+    cpu_temp = float(f.readline().split()[0])
+    payload["cpu_temp"] = cpu_temp
+
+# important processes
+process_cmds = [ 
+    { 'name': 'bipod_pid', 'cmd' : ['pgrep', '-f', 'bipod.py' ] },
+    { 'name': 'linuxcnc_pid', 'cmd': ['pgrep', '-f', 'linuxcnc bipod.ini'] },
+    { 'name': 'autossh_pid', 'cmd': ['pgrep', 'autossh'] }, ]
+
+for process in process_cmds:
+    p = Popen(process['cmd'], stdout=PIPE, stderr=PIPE)
+    stdout, err = p.communicate()
+    try:
+        pid = int(stdout)
+        payload[process['name']] = True
+    except ValueError:
+        payload[process['name']] = False
+
+# log it
+url = 'http://phant.cursivedata.co.uk/input/3Nx3P463ZJhKwXqzNJKbCJqg13r'
 r = requests.get(url, params=payload)
 log.debug("get status = %d" % r.status_code)
 
